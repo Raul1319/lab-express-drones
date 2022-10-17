92226e5cabf41257c559036bd0a4d22494753ad0
Vicky1719 <112522032+Vicky1719@users.noreply.github.com>
2022-10-17 17:48:19 +0200

    iteracion1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,13 @@
-// Iteration #1
+const { default: mongoose } = require("mongoose")
+const mogoose = require("mongoose")
+
+const droneSchema = new mongoose.Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+})
+
+
+const Drone = mongoose.model("Drone", droneSchema);
+
+module.exports = Drone
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,32 @@
 // Iteration #1
+
+
+const mongoose = require("mongoose");
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+ require("../db")
+  const Drone = require ("../models/Drone.model.js");
+ Drone
+ .create(drones)
+ .then(() => {
+
+ })
+.catch((err) => {
+
+})
+
+
+mongoose.connection.close()
+
+
+
+  
+
+
+  
+
+
